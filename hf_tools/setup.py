302791e53e153c4b974d44e91386b0f493b762b0
uwe <uwe90711@gmail.com>
2024-04-02 14:29:20 +0800

    fix: fixed setup tools

--- hf_tools/setup.py
+++ hf_tools/setup.py
@@ -1,10 +1,28 @@
-## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+from setuptools import setup, find_packages
 
-# fetch values from package.xml
-setup_args = generate_distutils_setup(
-	packages=['hf_tools'],
-	package_dir={'': 'src'},
+setup(
+    name="hf_tools",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[
+        'huggingface_hub',
+        'datasets'
+    ],
+    # Optional
+    author="uwe",
+    author_email="uwe90711@gmail.com",
+    description="A tools for easier use of huggingface_hub",
+    keywords="hf tool",
+    url="https://github.com/ARG-NCTU/uav-usv-traj.git",
 )
-setup(**setup_args)
+
+# ## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+# from distutils.core import setup
+# from catkin_pkg.python_setup import generate_distutils_setup
+
+# # fetch values from package.xml
+# setup_args = generate_distutils_setup(
+# 	packages=['hf_tools'],
+# 	package_dir={'': 'src'},
+# )
+# setup(**setup_args)
